1f15013bbf2f3fd8115865c656a9b0ab72ab08ae
lukasjoc <45790416+lukasjoc@users.noreply.github.com>
2024-10-06 17:06:21 +0200

    feat: add verbose and eval flag to cli

--- prec.go
+++ prec.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"errors"
+	"flag"
 	"fmt"
 	"io"
 	"math/big"
@@ -12,7 +13,12 @@ import (
 	"golang.org/x/term"
 )
 
-func main() {
+var (
+	verbose     = flag.Bool("v", false, "Be verbose with the output")
+	evalContent = flag.String("e", "", "Directly evaluate an expression and print the result to stdout")
+)
+
+func openRepl() {
 	old, err := term.MakeRaw(int(os.Stdin.Fd()))
 	if err != nil {
 		panic(fmt.Errorf("could not enter raw mode: %w", err))
@@ -48,13 +54,63 @@ func main() {
 			fmt.Print("\033[2K\r")
 			continue
 		}
-		s.Visit(func(s *sexpr.SExpr, ctx *sexpr.SExprVisitCtx) {
-			if s == nil {
-				return
-			}
-			fmt.Printf("%s%s\n", strings.Repeat(strings.Repeat(" ", 2), int(ctx.Depth)), s.String())
+		if verbose != nil && *verbose == true {
+			s.Visit(func(s *sexpr.SExpr, ctx *sexpr.SExprVisitCtx) {
+				if s == nil {
+					return
+				}
+				fmt.Printf("%s%s\n", strings.Repeat(strings.Repeat(" ", 2), int(ctx.Depth)), s.String())
+				fmt.Print("\033[2K\r")
+			}, &sexpr.SExprVisitCtx{Depth: 0})
+		}
+		val, err := s.Eval()
+		if err != nil {
+			fmt.Printf("Error: %v\n", err)
 			fmt.Print("\033[2K\r")
-		}, &sexpr.SExprVisitCtx{Depth: 0})
+		}
+		if val != nil {
+			prec := int(val.Prec())
+			acc := val.Acc()
+			if acc == big.Exact {
+				fmt.Printf("%s ", acc)
+				if val.IsInt() {
+					prec = 0
+				} else {
+					prec = 1
+				}
+			}
+			fmt.Println(val.Text('f', prec))
+			fmt.Print("\033[2K\r")
+		}
+	}
+}
+
+func main() {
+	flag.Parse()
+	if evalContent == nil || len(*evalContent) == 0 {
+		openRepl()
+	} else {
+		line := *evalContent
+		builder := sexpr.NewBuilder(line)
+		s, err := builder.Build()
+		if errors.Is(err, io.EOF) {
+			os.Exit(1)
+		}
+		if err != nil {
+			fmt.Printf("Error: %v\n", err)
+			fmt.Print("\033[2K\r")
+			os.Exit(1)
+		}
+
+		if verbose != nil && *verbose == true {
+			s.Visit(func(s *sexpr.SExpr, ctx *sexpr.SExprVisitCtx) {
+				if s == nil {
+					return
+				}
+				fmt.Printf("%s%s\n", strings.Repeat(strings.Repeat(" ", 2), int(ctx.Depth)), s.String())
+				fmt.Print("\033[2K\r")
+			}, &sexpr.SExprVisitCtx{Depth: 0})
+		}
 		val, err := s.Eval()
 		if err != nil {
 			fmt.Printf("Error: %v\n", err)
